a708b88c547add34afa152d4585a404cc7503fab
perneszmatyi <perneszmatyi@gmail.com>
2025-02-05 10:24:25 +0100

    switched firebase api

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -4,12 +4,13 @@ import Constants from 'expo-constants';
 
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAnNcxiRjVOoVpvp296vWdRbsS6Mu-492s",
-  authDomain: "szedui.firebaseapp.com",
-  projectId: "szedui",
-  storageBucket: "szedui.firebasestorage.app",
-  messagingSenderId: "235114494776",
-  appId: "1:235114494776:web:48885d2af631d9a45fdfcd"
+  apiKey: "AIzaSyDtY_0v5-upDj2-Qlcvp9Wga8EFpAIwLqI",
+  authDomain: "driver-behavior-test-app.firebaseapp.com",
+  projectId: "driver-behavior-test-app",
+  storageBucket: "driver-behavior-test-app.firebasestorage.app",
+  messagingSenderId: "285918275837",
+  appId: "1:285918275837:web:93948cb85ee45c09583694",
+  measurementId: "G-7SVP138816"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApp();
